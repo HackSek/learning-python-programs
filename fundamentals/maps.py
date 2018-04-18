42a916dea00247c8fcdadf5e59913ace37b65606
Hachem <HackSek@users.noreply.github.com>
2018-04-18 05:17:58 +0100

    learned about maps

---
+++ fundamentals/maps.py
@@ -0,0 +1,31 @@
+from random import shuffle
+
+def shuffle_a_word(argument):
+    word_to_shuffle = list(argument)
+    shuffle(word_to_shuffle)
+    return ''.join(word_to_shuffle)
+
+words_to_shuffle = list()
+
+while True:
+    add_a_word = input('what word would you like to shuffle? ')
+    add_another = input('would you like to add another word? (y/n) ')
+    if add_another == 'n':
+        words_to_shuffle.append(add_a_word)
+        break
+    else:
+        words_to_shuffle.append(add_a_word)
+        continue
+
+# shuffled_words = list()
+#
+# for word_to_shuffle in words_to_shuffle:
+#     shuffled_words.append(shuffle_a_word(word_to_shuffle))
+#
+# print(shuffled_words)
+
+# print(list(map(shuffle_a_word, words_to_shuffle)))
+
+shuffled_words = [ shuffle_a_word(word_to_shuffle) for word_to_shuffle in words_to_shuffle ]
+
+print(shuffled_words)
